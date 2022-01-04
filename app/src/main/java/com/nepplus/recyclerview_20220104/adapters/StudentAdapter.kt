7cca4fd5efe90831a9bdfbe8454ed57276ffecea
Lee Seung Hun <negrat@naver.com>
2022-01-04 11:16:22 +0900

    어댑터 내부 전용 클래스 ViewHolder 상속받아 추가     - 존재 명시만 함. 완성 X

--- app/src/main/java/com/nepplus/recyclerview_20220104/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/recyclerview_20220104/adapters/StudentAdapter.kt
@@ -6,4 +6,10 @@ import androidx.recyclerview.widget.RecyclerView
 // 안드로이드에서 기초 작업을 제공 => 상속을 받아서 일부 수정
 
 class StudentAdapter : RecyclerView.Adapter<>() {
+
+//      StudentAdapter 클래스 안에서, 내부 전용 클래스 추가 생성.
+    inner class MyViewHolder : RecyclerView.ViewHolder() {
+
+    }
+
 }
